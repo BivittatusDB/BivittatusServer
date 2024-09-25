0f3b8f054d3549def339e41ec59cca5523d2ff78
HarbingerOfFire <HarbingerOfFire@proton.me>
2024-09-25 15:15:36 -0600

    Init commit with outline

---
+++ client.py
@@ -0,0 +1,87 @@
+import requests
+
+keywords=[
+    "TABLE",
+    "DATABASE",
+    "USER",
+    "USE",
+    "SET",
+    "SYS"
+]
+SESS=requests.Session()
+db=None
+tb=None
+
+class req:
+    def __init__(self, path) -> None:
+        self.root_path=path
+
+    def GET(self, data=..., var_name="query"):
+        if data:
+            SESS.get(self.root_path+f"?{var_name}={data}")
+        else:
+            SESS.get(self.root_path)
+
+    def POST(self, data):
+        SESS.post(self.root_path, json=data)
+
+    def PUT(self, data):
+        SESS.put(self.root_path, json=data)
+    
+    def DELETE(self):
+        SESS.delete(self.root_path)
+
+class TABLE(req):
+    def __init__(self) -> None:
+        super().__init__(f"http://localhost:5000/database/{db}/{tb}")
+
+class DATABASE(req):
+    def __init__(self) -> None:
+        super().__init__(f"http://localhost:5000/database/{db}")
+
+class USER(req):
+    def __init__(self, id:int|None=None) -> None:
+        super().__init__(f"http://localhost:5000/users/{id}")
+
+class SET(req):
+    def __init__(self, var) -> None:
+        super().__init__(f"http://localhost:5000/sys/{var}")
+
+class SYS(req):
+    def __init__(self) -> None:
+        super().__init__("http://localhost:5000/sys/exec")
+
+class USE:
+    #TODO: ADD checks
+    def database(self, name):
+        global db
+        db=name
+
+    def table(self, name):
+        global tb
+        tb=name
+
+def prompt():
+    if tb and db:
+        return f"bdb/{db}/{tb}>"
+    if db:
+        return f"bdb/{db}>"
+    else:
+        return f"bdb>"
+
+while True:
+    command=input(prompt())
+    for key in keywords:
+        if key in command:
+            command=command.split(" ")
+            if key=="USE":
+                exec(f"USE().{command[1]}(\"{command[2]}\")")
+                print(db, tb)
+            if key=="SET":
+                r=...
+                exec(f"SET(\"{command[1]}\").GET(\"{command[2]}\", 'value')")
+            if key=="SYS":
+                exec(f"SYS().GET(\"{" ".join(command[1:])}\")")
+            #TODO: ADD Table wrapper for api
+        else:
+            exec(command)
---
+++ server.py
@@ -0,0 +1,102 @@
+import flask, BivittatusDB as bdb, requests
+import requests.cookies
+
+app=flask.Flask(__name__)
+
+###################### USERS ######################
+#Get list of users when get method is called
+@app.route("/users", methods=['GET'])
+def GET_USER():
+    pass
+
+#Get user by ID
+@app.route("/users/<int:id>", methods=['GET'])
+def GET_USER_BY_ID(id:int):
+    pass
+
+#create new user when post method is called
+@app.route("/users", methods=['POST'])
+def MAKE_USER():
+    data=flask.request.get_json()
+    pass
+
+#Update user from id
+@app.route("/users/<int:id>", methods=['PUT'])
+def UPDATE_USER(id: int):
+    data=flask.request.get_json()
+    pass
+
+#delete user by Id
+@app.route("/users/<int:id>", methods=["DELETE"])
+def DELETE_USER(id: int):
+    pass
+
+###################### DATABASE ######################
+#get a list of databases
+@app.route("/database", methods=["GET"])
+def GET_DATABASE():
+    pass
+
+#get list of tables from database
+@app.route("/database/<string:basename>", methods=["GET"])
+def GET_TABLES(basename:str):
+    pass
+
+#create a new database
+@app.route("/database/<string:basename>", methods=["POST"])
+def CREATE_DATABASE(basename:str):
+    data=flask.request.get_json()
+    pass
+
+#delete a database by name
+@app.route("/database/<string:basename>", methods=["DELETE"])
+def DELETE_DATABASE(basename:str):
+    pass
+
+#############################################
+# I don't have a "PUT" method for databases #
+# yet cause I don't have a viable idea for  #
+# what updating the database (as opposed    #
+# to updating a table) is. Our library only #
+# has get and make type methods. Possibly   #
+# something to look into.                   #
+#############################################
+
+###################### TABLES ######################
+#Return database data as JSON
+@app.route("/database/<string:basename>/<string:table>", methods=["GET"])
+def GET_DATA(basename:str, table:str):
+    pass
+
+@app.route("/database/<string:basename>/<string:table>", methods=["POST"])
+def MAKE_TABLE(basename:str, table:str):
+    data=flask.request.get_json()
+    pass
+
+@app.route("/database/<string:basename>/<string:table>", methods=["PUT"])
+def UPDATE_TABLE(basename:str, table:str):
+    data=flask.request.get_json()
+    pass
+
+@app.route("/database/<string:basename>/<string:table>", methods=["DELETE"])
+def DELETE_TABLE(basename:str, table:str):
+    pass
+
+###################### SYS ######################
+# set a system variable using localhost:/sys/<varaible_name>?value=<Value>
+@app.route("/sys/<string:var>", methods=["GET"])
+def SET_VAR(var:str):
+    if flask.request.args.get('value'):
+        resp=flask.make_response()
+        resp.set_cookie(var, flask.request.args.get('value'))
+        return resp
+    else:
+        r=flask.request.cookies.get(var)
+        return r
+
+@app.route("/sys/exec")
+def EXEC():
+    exec(flask.request.args.get('query'))
+
+if __name__=='__main__':
+    app.run()
